e327432b5b95fd219fe09ad5b52ecf575448ec83
Francisco <56081472+REPTaiLE@users.noreply.github.com>
2023-09-04 18:53:36 -0300

    Lista de Tareas

---
+++ listaDeTareas/script.js
@@ -0,0 +1,74 @@
+// Selección de Elementos
+
+const input = document.getElementById('ingresar-tarea');
+const boton = document.querySelector('button');
+const listaDeTarea = document.getElementById('lista-de-tareas');
+
+
+
+// Definiendo funciones 
+
+function agregarTarea() {
+    if (input.value) {
+        // Creación de tarea
+        let tareaNueva = document.createElement('div');
+        tareaNueva.classList.add('tarea');
+        
+        // Texto ingresado por el usuario
+        let texto = document.createElement('p');
+        texto.innerText = input.value;
+        tareaNueva.appendChild(texto);
+
+        // Crear y agregar contenedor de íconos
+        let iconos = document.createElement('div');
+        iconos.classList.add('iconos');
+        tareaNueva.appendChild(iconos);
+
+        // Íconos
+        let completar = document.createElement('i');
+        completar.classList.add('bi', 'bi-check-circle-fill', 'icono-completar');
+        completar.addEventListener('click', completarTarea);
+
+        let eliminar = document.createElement('i');
+        eliminar.classList.add('bi', 'bi-trash3-fill', 'icono-eliminar');
+        eliminar.addEventListener('click', eliminarTarea)
+
+        iconos.append(completar, eliminar);
+
+        // Agregar tarea a la lista
+        listaDeTarea.appendChild(tareaNueva);
+
+    } else {
+        alert('Por favor ingresa una tarea!');
+    }
+
+}
+
+
+// Funciones manipulación de tareas
+
+
+// Completar tarea
+
+function completarTarea(e) {
+    let tarea = e.target.parentNode.parentNode;
+    tarea.classList.toggle('completada');
+}
+
+
+// Eliminar tarea
+
+function eliminarTarea(e) {
+    let tarea = e.target.parentNode.parentNode;
+    tarea.remove();
+}
+
+
+// Agregar detector de eventos
+
+boton.addEventListener('click', agregarTarea);
+input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        agregarTarea();
+    }
+});
